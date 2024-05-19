0851cfb726f23427b50cb79ad6c450aae090b74c
Erik Villarreal <erik.villarreal@uab.cat>
2024-05-19 07:36:16 +0200

    merge routes into 1 endpoint

--- main.py
+++ main.py
@@ -18,12 +18,20 @@ def precompute_data():
             "total_days": total_days
         }
 
-@app.route("/route1")
-def route1():
-	ordered_coordinates2 = precomputed_data[2]["ordered_coordinates"]
-	total_days2 = precomputed_data[2]["total_days"]
 
-	df = pd.read_csv("Dades_Municipis_Lot_2.csv")
+LOTS = [2, 4, 5];
+N_WEEKS = 4;
+N_DAYS = 5;
+
+@app.route("/lot/{lot}")
+def getRoute(lot):
+	if not lot in LOTS:
+		return
+	
+	ordered_coordinates = precomputed_data[lot]["ordered_coordinates"]
+	total_days = precomputed_data[lot]["total_days"]
+
+	df = pd.read_csv(f"Dades_Municipis_Lot_{lot}.csv")
 	locations = {}
 	for town, pob, coord in zip(df["Municipi"], df["Pob."], df["coordinates"]):
 		if not pob.is_integer():
@@ -31,41 +39,7 @@ def route1():
 		locations[coord] = [town, int(pob)]
 
 	towns = []
-	for coord in ordered_coordinates2:
-		towns.append({"location":locations[str(coord)][0], "population":locations[str(coord)][1], "coordinates":coord})
-	return json.dumps(towns)
-
-@app.route("/route2")
-def route2():
-	ordered_coordinates4 = precomputed_data[4]["ordered_coordinates"]
-	total_days4 = precomputed_data[4]["total_days"]
-
-	df = pd.read_csv("Dades_Municipis_Lot_4.csv")
-	locations = {}
-	for town, pob, coord in zip(df["Municipi"], df["Pob."], df["coordinates"]):
-		if not pob.is_integer():
-			pob *= 1000
-		locations[coord] = [town, int(pob)]
-
-	towns = []
-	for coord in ordered_coordinates4:
-		towns.append({"location":locations[str(coord)][0], "population":locations[str(coord)][1], "coordinates":coord})
-	return json.dumps(towns)
-
-@app.route("/route3")
-def route3():
-	ordered_coordinates5 = precomputed_data[5]["ordered_coordinates"]
-	total_days5 = precomputed_data[5]["total_days"]
-
-	df = pd.read_csv("Dades_Municipis_Lot_5.csv")
-	locations = {}
-	for town, pob, coord in zip(df["Municipi"], df["Pob."], df["coordinates"]):
-		if not pob.is_integer():
-			pob *= 1000
-		locations[coord] = [town, int(pob)]
-
-	towns = []
-	for coord in ordered_coordinates5:
+	for coord in ordered_coordinates:
 		towns.append({"location":locations[str(coord)][0], "population":locations[str(coord)][1], "coordinates":coord})
 	return json.dumps(towns)
 
